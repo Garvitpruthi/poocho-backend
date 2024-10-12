8122904c58fa56b51170d79ce2b62889945bb65f
Garvit Pruthi <garvitpruthi9817@gmail.com>
2024-10-13 04:12:53 +0530

    Added basic server setup, nodemon, dotenv

---
+++ src/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const { PORT } = require('./config/server.config');
+const bodyParser = require('body-parser');
+
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.text());
+
+
+app.listen(PORT, ()=>{
+    console.log(`server started at port ${PORT}`)
+})
